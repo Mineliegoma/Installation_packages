512f5cd059522799b531f9dfcf54fba25c3b1d4c
root <root@localhost.localdomain>
2022-11-05 01:34:58 +0000

    jenkins installation

--- Jenskins_Packages.sh
+++ Jenskins_Packages.sh
@@ -2,10 +2,34 @@
 
 #Author : Minelie 
 
-#Date:
-
-########"
+#Date: Nov 04 2022 
 
 
+#################
+echo "Starting Jenskins Installation"
 
+ wget -O /etc/yum.repos.d/jenkins.repo \
+ https://pkg.jenkins.io/redhat-stable/jenkins.repo
+ rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+ yum upgrade
+# Add required dependencies for the jenkins package
+ yum install java-11-openjdk
+ yum install jenkins -y
+ systemctl daemon-reload
+systemctl enable jenkins
+systemctl start jenkins
+systemctl status jenkins
+echo "Http installation, starting and status" 
+yum install httpd -y
+systemctl status httpd
+systemctl start httpd
+systemctl enable httpd
+echo "Firewall starting"
+systemctl start firewalld
+systemctl status firewalld
+sleep 3
+firewall-cmd --permanent --zone=public --add-port=8080/tcp
+firewall-cmd --reload
+sleep 3
+echo "Jenkins is installed!!"
 
